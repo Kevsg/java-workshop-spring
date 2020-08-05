32f25ea974a3b790096cc6258f1e47e8d62fe3c9
Phongpak Taothong <phongpaktao@mathregit.group>
2020-08-05 16:23:21 +0700

    Step05

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -16,15 +19,18 @@ public class UserController {
     public PagingResponse getPage(@RequestParam(defaultValue = "1") int page, @RequestParam(name = "item_per_page", defaultValue = "10") int itemPerPage) {
         PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
         List<UserResponse> usersResponseList = new ArrayList<>();
-        usersResponseList.add(new UserResponse(1, "User 1"));
-        usersResponseList.add(new UserResponse(2, "User 2"));
+        Page<User> result = userRepository.findAll(PageRequest.of(page-1, itemPerPage));
+        for(User u: result.getContent()) {
+            usersResponseList.add(new UserResponse(u.getId(),u.getName(), u.getAge()));
+        }
         pagingResponse.setUsersResponseList(usersResponseList);
         return pagingResponse;
     }
 
     @GetMapping("/users/{id}")
     public UserResponse getUserById(@PathVariable int id) {
-        return new UserResponse(id, "User " + id);
+        Optional<User> user = userRepository.findById(id);
+        return new UserResponse(user.get().getId(), user.get().getName(), user.get().getAge());
     }
 
     @PostMapping("/users")
@@ -33,7 +39,7 @@ public class UserController {
         user.setName(request.getName());
         user.setAge(request.getAge());
         user = userRepository.save(user);
-        return new UserResponse(user.getId(),user.getName()+user.getAge());
+        return new UserResponse(user.getId(),user.getName()+user.getAge(), user.getAge());
     }
 
 }
--- src/main/java/com/example/demo/UserResponse.java
+++ src/main/java/com/example/demo/UserResponse.java
@@ -4,10 +4,16 @@ public class UserResponse {
 
     private int id;
     private String name;
+    private int age;
 
     public UserResponse(int id, String name) {
+        this(id,name,0);
+    }
+
+    public UserResponse(int id, String name,int age) {
         this.id = id;
         this.name = name;
+        this.age = age;
     }
 
     public int getId() {
